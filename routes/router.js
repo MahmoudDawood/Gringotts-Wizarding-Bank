c447b40f376f21f933d0cac906118b952b49a15e
MahmoudDawood <mm.dawood16@yahoo.com>
2022-02-12 21:40:58 +0200

    Process users transactions and save to database

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,47 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dbURL = require('../config')
+const User = require('../models/user')
+const Process = require('../models/process')
+
+// Connect to MongoDB
+const connection = mongoose.connect(dbURL)
+    .then((res) => console.log('Connected!'))
+    .catch((err) => console.log(err))
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+// Allow passing url encoded data into object
+app.use(express.urlencoded({ extended: true }))
+
+// Middleware for loading users data
+let Users
+app.use((req, res, next) => {
+    User.find()
+        .then((result) => {
+            Users = result
+        })
+        .catch((err) => console.log(err))
+    next()
+})
+
+// App routes
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Home',
         name: 'Home Page'
     })
+})
+
+app.get('/print', (req, res) => {
+    res.send(Users)
+    // res.render('customers', {
+    //     title: 'Customers',
+    //     name: 'Customers Page',
+    //     customers: Users
+    // }).catch((err) => console.log(err))
 })
 
 app.get('/about', (req, res) => {
@@ -18,41 +51,65 @@ app.get('/about', (req, res) => {
     })
 })
 
-const customers = [
-    {
-        name: 'James',
-        balance: 5000
-    },
-    {
-        name: 'Lilly',
-        balance: 2000
-    },
-    {
-        name: 'Harry',
-        balance: 10000
-    }
-]
+app.get('/customers', (req, res) => {   
+    // re running middleware
+    User.find()
+        .then((customers) => {
+            res.render('customers', {
+                title: 'Customers',
+                name: 'Customers Page',
+                customers
+            })
+        })
 
-app.get('/customers', (req, res) => {
-    res.render('customers', {
-        title: 'Customers',
-        name: 'Customers Page',
-        customers
-    })
 })
 
-app.get('/customers/customer', (req, res) => {
-    res.render('index', {
-        title: 'Customer',
-        name: 'Customer Page'
-    })
+app.get('/customers/:id', (req, res) => {
+    const customers = Users.filter((customer) => customer._id.valueOf() !== req.params.id)
+    User.findById(req.params.id)
+        .then((customer) => {
+            res.render('customer', {
+                title: 'Customer',
+                name: 'Customer Page',
+                customers,
+                customer
+            })
+        })
+            
 })
 
-app.get('/customers/customer/transfer', (req, res) => {
-    res.render('index', {
-        title: 'Transfer',
-        name: 'Money Transfer Page'
-    })
+app.post('/customers', (req, res) => {
+    User.findById(req.body.source)
+        .then((sender) => {
+            const money = req.body.amount
+            if (sender.balance < money) return
+            User.findById(req.body.target)
+                .then(async(reciever) => {
+                    reciever.balance += Number(money)
+                    sender.balance -= Number(money)
+                    await sender.save()
+                    await reciever.save()
+                    const process = new Process({
+                        source: sender.name,
+                        target: reciever.name,
+                        amount: money
+                    })
+                    await process.save()
+                    res.redirect('/customers')
+                })
+        })
+        .catch((err) => console.log(err))
+})
+
+app.get('/history', (req, res) => {
+    Process.find()
+        .then((process) => {
+            res.render('history', {
+                title: 'History',
+                name: 'Transaction History',
+                process
+            })
+        })
 })
 
 app.get('*', (req, res) => {
